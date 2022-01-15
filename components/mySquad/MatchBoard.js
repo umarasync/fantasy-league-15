4a801f10e567b29f7cff071aee9effac5b96f03e
Umar Farooq <umarmughal1194@gmail.com>
2022-01-15 06:27:40 +0500

    v5

--- components/mySquad/MatchBoard.js
+++ components/mySquad/MatchBoard.js
@@ -1,6 +1,6 @@
 // Packages
 import {motion, useAnimation} from "framer-motion";
-import {createRef, useEffect, useRef, useState} from "react";
+import {createRef, useEffect, useMemo, useRef, useState} from "react";
 
 // Components
 import Div from "components/html/Div";
@@ -10,6 +10,7 @@ import Image from "components/html/Image";
 // Constants
 import {SHADOW_WHITE_SMOKE} from "constants/data/boxShadow";
 import colors from "constants/colors";
+import {MATCHES} from "constants/data/matches";
 
 // utils
 import {clone} from "utils/helpers";
@@ -25,12 +26,13 @@ const getStyles = (R) => {
         scrollContainer:{
             whiteSpace: 'nowrap',
             // overflow: 'hidden',
-            background: 'red',
+            // background: 'red',
             // height: 80,
             // paddingLeft: R(167),
             // paddingRight: R(166),
             // height: 'max-content',
-            width: R(947)
+            // width: R(947)
+            width: R(1080)
             // width: R(1107)
             // marginLeft: R(-10)
         },
@@ -40,115 +42,74 @@ const getStyles = (R) => {
             display: 'inline-block',
             textAlign: 'center',
             background: 'white',
-            // border: '1px solid red',
-            // width: R(189.4)
+            // marginLeft: R(32),
+            // marginRight: R(32),
+            marginLeft: R(35),
+            marginRight: R(35),
+
+            opacity: 0,
+        },
+        subHeading: {
+            color: colors.regent_grey,
+            fontSize: R(28),
+            lineHeight: R(32, 'px')
+        },
+        borderStyle: {
+            height: R(2),
+            position: 'absolute',
+            background: colors.mandy,
+            marginTop: R(20),
         }
     }
 }
-
-const data = [
-    {
-        id: 1,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false,
-        firstChild: true,
-    },
-    {
-        id: 111,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false
-    },
-    {
-        id: 2,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false,
-    },
-    {
-        id: 3,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false
-    },
-    {
-        id: 4,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false
-    },
-    {
-        id: 5,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false
-    },
-    {
-        id: 6,
-        week: 'Gameweek 8',
-        date: 'Make transfers',
-        active: true
-    },
-    {
-        id: 7,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false,
-        lastChild: true
-    },
-    {
-        id: 8,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false
-    },
-    {
-        id: 9,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false
-    },
-    {
-        id: 10,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false
-    },
-    {
-        id: 11,
-        week: 'Gameweek 8',
-        date: '29 sep',
-        active: false
-    },
-
-]
 
 export default function MatchBoard () {
 
     const STYLES =  { ...getStyles(R) }
 
-    const INITIAL_MATCHES = clone(data)
+    const INITIAL_MATCHES = clone(MATCHES)
 
-    const [matches, setMatches] = useState(INITIAL_MATCHES)
+    const [matches, setMatches] = useState([])
 
     const scrollContainer = useRef()
     let activeRef = useRef()
 
     const controls = useAnimation()
+    const borderAnimationControls = useAnimation()
 
     const [moved, setMoved] = useState(0)
-    const [firstLoad, setFirstLoad] = useState(true)
+    const [initialRenderDone, setInitialRenderDone] = useState(false)
     const [animationInProgress, setAnimationInProgress] = useState(false)
     const [areElementsPositionSet, setAreElementsPositionSet] = useState(false)
     const [borderWidth, setBorderWidth] = useState(0)
 
-
-
     const [rights, setRights] = useState([]);
-    const elementsRef = useRef(data.map(() => createRef()));
+    const elementsRef = useRef(INITIAL_MATCHES.map(() => createRef()));
+
+    // const calculateAllRefs = ($matches) => {
+    //     const nextRights = elementsRef.current.map(ref => {
+    //         if(ref.current){
+    //             return getActiveRect(ref)
+    //         }else if(activeRef){
+    //             return getActiveRect(activeRef)
+    //         }
+    //     });
+    //
+    //     const $matchesI = $matches.map((match, index) => {
+    //         match.elementPosition = nextRights[index]
+    //         return match
+    //     })
+    //
+    //     setMatches($matchesI)
+    //     setRights(nextRights);
+    //     setAreElementsPositionSet(true)
+    //     // handleScroll()
+    // }
+
+
 
     const calculateAllRefs = ($matches) => {
+
         const nextRights = elementsRef.current.map(ref => {
             if(ref.current){
                 return getActiveRect(ref)
@@ -157,33 +118,61 @@ export default function MatchBoard () {
             }
         });
 
-        const $matchesI = $matches.map((match, index) => {
-            match.elementPosition = nextRights[index]
-            return match
-        })
-
-        setMatches($matchesI)
-        setRights(nextRights);
-        setAreElementsPositionSet(true)
-        // handleScroll()
+        if(!rights.length){
+            setRights(nextRights)
+        } else if(rights[0].activeLeft !== nextRights[0].activeLeft) {
+            setRights(nextRights);
+        }
     }
 
+    useEffect(()=> {
+
+        let currentActive = matches.findIndex((match) => match.active)
+
+        const $matches = matches.map((item, index) => {
+            const elPos = rights[index]
+            item.overflowing = true
+            if(elPos && elPos.activeLeft > 0 && elPos.activeLeft < 947 && elPos.activeRight < 0 && elPos.activeRight > -947) {
+                item.overflowing = false
+            }
+            return item
+        })
+
+         setMatches($matches)
+
+    }, [rights])
 
     const scrollBoxOriginPointForBorder = R(517.421)
-    const scrollBoxOriginPoint = R(350.421)
+    // const scrollBoxOriginPoint = R(350.421)
+    const scrollBoxOriginPoint = R(417)
 
     const duration = 0.7
+    // const duration = 3
 
-    const scrollAnimationVariant = {
-        scroll: {
-            x: -moved,
-            transition: {
-                duration: duration,
-            },
+    const getOpacity = (match) => {
+        if(match.active || match.lastActive) {
+            return 1
+        }else if(!match.overflowing) {
+            return 0.5
+        }else {
+            return 0.1
+        }
+
+    }
+
+    const scrollAnimation = {
+        scroll: (match) => {
+            return {
+                x: -moved,
+                opacity: getOpacity(match),
+                transition: {
+                    duration: duration + 0.2,
+                },
+            }
         },
     };
 
-    const borderAnimationVariant = {
+    const borderAnimation = {
         borderWidth: {
             width: borderWidth,
             transition: {
@@ -192,9 +181,28 @@ export default function MatchBoard () {
         }
     };
 
+    const subHeadingAnimation = {
+        changeTextColor: (match) => {
+            return {
+                color: match.active ? colors.black_rock : colors.regent_grey,
+                transition: {
+                    duration: duration
+                }
+            }
+        }
+    };
+
+    useEffect(() => {
+        if(initialRenderDone){
+            controls.start('scroll')
+            borderAnimationControls.start('borderWidth')
+            controls.start('changeTextColor')
+        }
+    }, [moved])
+
     const getActiveRect = (itemRef) => {
 
-        if(!itemRef) return;
+        if(!itemRef.current) return;
 
         const scrollRect = scrollContainer.current.getBoundingClientRect()
         const activeRect = itemRef.current.getBoundingClientRect()
@@ -210,6 +218,14 @@ export default function MatchBoard () {
 
         const { activeLeft, activeRect, activeRight} = getActiveRect(activeRef)
 
+        console.log('data=======', {
+            activeLeft: parseInt(activeLeft),
+            rights,
+            scrollBoxOriginPoint: parseInt(scrollBoxOriginPoint)
+        })
+
+        if(animationInProgress || parseInt(activeLeft) === parseInt(scrollBoxOriginPoint)) return
+
         setBorderWidth(activeRect.width)
 
         let movedPixels = 0;
@@ -219,131 +235,112 @@ export default function MatchBoard () {
         }else {
             movedPixels = -1 * (scrollBoxOriginPoint - activeLeft)
         }
-
-        setFirstLoad(false)
         setMoved(moved + movedPixels)
-
     }
 
-    const handleOnClick = (match) => {
-        const $matches = matches.map((item) => {
+    const handleTabClick = (match) => {
+        let currentActive = matches.findIndex((match) => match.active)
+
+        const $matches = matches.map((item, index) => {
+
+            const elPos = rights[index]
+
             item.active = item.id === match.id;
+            item.lastActive = index === currentActive
+
+            item.overflowing = !(elPos && elPos.activeLeft > 0 && elPos.activeLeft < 947 && elPos.activeRight < 0 && elPos.activeRight > -947);
+
             return item
         })
-        setFirstLoad(false)
-        // setMatches($matches)
-        calculateAllRefs($matches)
+
+        setMatches($matches)
     }
-
-    // useEffect(() => {
-    //     if(!firstLoad){
-    //         handleScroll()
-    //     }
-    // }, [matches, firstLoad])
-
-    useEffect(() => {
-        if(!firstLoad){
-            handleScroll()
-        }
-    }, [firstLoad])
-
-    useEffect(() => {
-        if(!firstLoad){
-            controls.start('scroll')
-            controls.start('borderWidth')
-        }
-    }, [moved])
-
-    const initialRender = () => {
-
-        setTimeout(() => {
-            const { activeRect } = getActiveRect(activeRef)
-            setBorderWidth(activeRect.width)
-            // handleScroll()
-            // setTimeout(() => {
-            //     calculateAllRefs()
-            // }, 1000)
-            setFirstLoad(false)
-        }, 300)
-    }
-    useEffect(() => {
-        initialRender()
-    }, [])
 
     const handleControls = (isNext = false) => {
+
         if(animationInProgress) return;
+
         const $matches = clone(matches)
         let objIndex = $matches.findIndex((match) => match.active)
+
         let nextIndex = isNext ? objIndex + 1 : objIndex - 1
+
         if(nextIndex === $matches.length || nextIndex === -1) return
-        handleOnClick($matches[nextIndex])
+
+        handleTabClick($matches[nextIndex])
     }
+
+    useEffect(() => {
+        const activeObj = matches.filter(match => match.active)
+        setInitialRenderDone(true)
+        if(initialRenderDone){
+         setTimeout(() => {
+             const { activeRect } = getActiveRect(activeRef)
+             setBorderWidth(activeRect.width)
+             handleScroll()
+         }, 50)
+        }
+    }, [matches, initialRenderDone])
+
+    useEffect(() => {
+        const $matches = INITIAL_MATCHES.map((item, index) => {
+            item.active = index === 5;
+            return item
+        })
+        setMatches($matches)
+    }, [])
 
     const onAnimationComplete = (definition) => {
-        if(definition === 'scroll') {
-            setAnimationInProgress(false)
-        }
-    }
-
-
-    const onBorderAnimationComplete = (definition) => {
-        calculateAllRefs(matches)
         if(definition === 'borderWidth') {
             setAnimationInProgress(false)
+            calculateAllRefs()
         }
-    }
-
-    const getOpacity = (match) => {
-        const elPos = match.elementPosition;
-
-        if(elPos && elPos.activeLeft > 0 && elPos.activeLeft < 947 && elPos.activeRight < 0 && elPos.activeRight > -947) {
-            return 1
-            console.log('true=========', elPos)
-        }
-
-        console.log('false=========', elPos)
-
-        return 0.5
     }
 
 
     return (
-        <Div h={720} pt={40} w={1280} style={STYLES.container}  className={'bg-red-200'} position="relative"  br={12} bs={SHADOW_WHITE_SMOKE}>
+        <Div h={720} pt={40} w={1280} style={STYLES.container}
+             className={'bg-white'}
+             position="relative" br={12} bs={SHADOW_WHITE_SMOKE}>
             <Div className={'flex justify-center'}>
                 <div style={{...STYLES.scrollContainer}}
                     // className={'flex justify-between'}
                      ref={scrollContainer}>
                     {
-                        matches.map((match, index) => {
+                        matches.length > 0 && matches.map((match, index) => {
                             return (
                                 <motion.div
-                                    variants={scrollAnimationVariant}
+                                    variants={scrollAnimation}
                                     animate={controls}
-                                    onAnimationStart={() => setAnimationInProgress(true)}
-                                    onAnimationComplete={(definition) => onAnimationComplete(definition)}
+                                    custom={match}
                                     key={match.id}
                                     className={'flex flex-col items-center'}
-                                    style={{...
-                                            STYLES.item,
-                                        marginLeft: index ? R(32)  : 0,
-                                        marginRight: index !== data.length -1 ? R(31) : 0,
-                                        background: match.active ? 'yellow': 'whitesmoke',
-                                        opacity: getOpacity(match)
+                                    style={{
+                                        ...STYLES.item,
+                                        opacity: match.overflowing ? 0 : 1
                                     }}
                                 >
                                     <div
                                         className={'flex flex-col items-center'}
                                         // ref={elementsRef.current[index]}
-                                        ref={ match.active ? activeRef : elementsRef.current[index] }
+                                        ref={match.active ? activeRef : elementsRef.current[index]}
                                         // ref={ match.active ? activeRef : null }
-                                        onClick={() => handleOnClick(match)}
+                                        onClick={() => handleTabClick(match)}
                                         data-lastChild={match.lastChild}
                                         data-firstChild={match.firstChild}
                                     >
-
                                         <Text text={match.week} color={colors.regent_grey} fs={18} lh={26}/>
-                                        <Text text={match.date} color={colors.regent_grey} fs={28} lh={32} fst={'italic'} tt={'uppercase'} fw={700}/>
-
+                                        <Text text={match.overflowing ? 'yes' : 'no'} color={colors.regent_grey}
+                                              fs={18} lh={26}/>
+                                        <motion.p
+                                            variants={subHeadingAnimation}
+                                            animate={controls}
+                                            custom={match}
+                                            className={'italic uppercase font-[700]'}
+                                            style={STYLES.subHeading}
+                                        >
+                                            {match.date}
+                                        </motion.p>
                                     </div>
 
                                 </motion.div>
@@ -353,26 +350,23 @@ export default function MatchBoard () {
                 </div>
             </Div>
 
-
             <Div position='absolute' top={40} left={40}>
-                <Image w={60} h={60} name={'arrow-prev.png'} cursor={'pointer'} bg={'green'} onClick={() => handleControls()}/>
-
+                <Image w={60} h={60} name={'arrow-prev.png'} cursor={'pointer'} onClick={() => handleControls()}/>
             </Div>
-
             <Div position='absolute' top={40} right={40}>
-                <Image w={60} h={60} name={'arrow-next.png'}  cursor={'pointer'} bg={'green'} onClick={() => handleControls(true)}/>
+                <Image w={60} h={60} name={'arrow-next.png'} cursor={'pointer'}
+                       onClick={() => handleControls(true)}/>
             </Div>
 
             <motion.div
-                variants={borderAnimationVariant}
-                onAnimationComplete={(definition) => onBorderAnimationComplete(definition)}
-                animate={controls}
+                variants={borderAnimation}
+                animate={borderAnimationControls}
+                onAnimationStart={() => setAnimationInProgress(true)}
+
+                onAnimationComplete={(definition) => onAnimationComplete(definition)}
                 style={{
+                    ...STYLES.borderStyle,
                     width: borderWidth,
-                    height: R(2),
-                    position: 'absolute',
-                    background: colors.mandy,
-                    marginTop: R(20),
                     left: scrollBoxOriginPointForBorder
                 }}
             />
---
+++ constants/data/matches.js
@@ -0,0 +1,58 @@
+export const MATCHES = [
+    {
+        id: 1,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+    {
+        id: 2,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+    {
+        id: 3,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+    {
+        id: 4,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+    {
+        id: 5,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+    {
+        id: 6,
+        week: 'Gameweek 8',
+        date: 'Make transfers',
+    },
+    {
+        id: 7,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+    {
+        id: 8,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+    {
+        id: 9,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+    {
+        id: 10,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+    {
+        id: 11,
+        week: 'Gameweek 8',
+        date: '29 sep',
+    },
+
+]
