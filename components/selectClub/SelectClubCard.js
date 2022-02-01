d1d0ef5f5d1df6da891af1e3a40623cfa185fbc7
Umar Farooq <umarmughal1194@gmail.com>
2022-02-02 00:02:58 +0500

    Fixed Height

--- components/selectClub/SelectClubCard.js
+++ components/selectClub/SelectClubCard.js
@@ -79,7 +79,7 @@ export default  function SelectClubCard({
         <img
             src={`/images/${bgImage}`}
             alt=""
-            className="w-[100%]"
+            className="w-[100%] h-[100%]"
         />
     </div>
     )}
