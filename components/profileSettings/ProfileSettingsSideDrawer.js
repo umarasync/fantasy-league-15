72a78ec3186e6b38cf2ba4dbb3904c3fa12ca3e7
Umar Farooq <umarmughal1194@gmail.com>
2022-02-12 22:31:22 +0500

    Added (null)

--- components/profileSettings/ProfileSettingsSideDrawer.js
+++ components/profileSettings/ProfileSettingsSideDrawer.js
@@ -54,6 +54,8 @@ export default function ProfileSettingsSideDrawer(){
         dispatch(toggleSideDrawer())
     }
 
+    return null
+
     return (
         <>
             <BackDrop show={showSideDrawer}/>
