d67b0e35b608d76e5b40962aad806b23d3c436a1
Umar Farooq <umarmughal1194@gmail.com>
2022-04-07 03:07:07 +0500

    save

--- components/buildTeam/BuildYourTeamPlayersPagination.js
+++ components/buildTeam/BuildYourTeamPlayersPagination.js
@@ -5,7 +5,6 @@ import {useDispatch, useSelector} from "react-redux";
 // Components
 import Div from "components/html/Div";
 import Text from "components/html/Text";
-import Button from "components/html/Button";
 import Image from "components/html/Image";
 
 // Actions
@@ -18,9 +17,6 @@ import {
     getPlayersStart
 } from "redux/Players/actionsCreators";
 
-// Constants
-import colors from "constants/colors";
-
 export default function BuildYourTeamPlayersPagination(){
 
     const dispatch = useDispatch()
@@ -28,6 +24,8 @@ export default function BuildYourTeamPlayersPagination(){
     const currentPage = useSelector(({ players }) => players.currentPage);
     const totalPages = useSelector(({ players }) => players.totalPages);
     const loadingPlayersGetting = useSelector(({ players }) => players.loadingPlayersGetting);
+    const disablePrev = currentPage < 1 || loadingPlayersGetting
+    const disableNext = currentPage === totalPages - 1 || loadingPlayersGetting
 
     const onPreviousPage = async () => {
         dispatch(getPlayersStart())
@@ -63,64 +61,46 @@ export default function BuildYourTeamPlayersPagination(){
     return (
         <Div center mb={20}>
             <Div center>
-                <Button
-                  title={'First'}
-                  disabled={currentPage < 1 || loadingPlayersGetting}
-                  color={colors.white}
-                  mr={8}
-                  h={50}
-                  w={100}
-                  onClick={onFirstPage}
-              />
                 <Image
-                    w={60}
-                    h={60}
-                    src={'/images/arrow-prev.png'}
-                    disabled={currentPage < 1 || loadingPlayersGetting}
+                    w={20}
+                    h={20}
+                    src={'/images/double-arrow-left.png'}
+                    disabled={disablePrev}
+                    cursor={'pointer'}
+                    onClick={onFirstPage}
+                    alt={'double-arrow-left'}
+                />
+                <Image
+                    w={50}
+                    h={50}
+                    src={'/images/angle-circle-left.png'}
+                    disabled={disablePrev}
                     cursor={'pointer'}
                     onClick={onPreviousPage}
-                    alt={'arrow-prev.png'}
+                    alt={'angle-circle-left'}
+                    ml={18}
                 />
-              {/*  <Button*/}
-              {/*    title={'Previous'}*/}
-              {/*    disabled={currentPage < 1 || loadingPlayersGetting}*/}
-              {/*    color={colors.white}*/}
-              {/*    ml={8}*/}
-              {/*    h={50}*/}
-              {/*    w={100}*/}
-              {/*    onClick={onPreviousPage}*/}
-              {/*/>*/}
             </Div>
             <Text ml={20} mr={20} text={`${currentPage + 1} of ${totalPages}`} fs={20} nowrap/>
             <Div center>
-
                 <Image
-                    w={60}
-                    h={60}
-                    src={'/images/arrow-next.png'}
-                    disabled={currentPage === totalPages - 1 || loadingPlayersGetting}
+                    w={50}
+                    h={50}
+                    src={'/images/angle-circle-right.png'}
+                    disabled={disableNext}
                     cursor={'pointer'}
                     onClick={onNextPage}
-                    alt={'arrow-next.png'}
+                    alt={'angle-circle-right'}
+                    mr={18}
                 />
-
-               {/* <Button*/}
-               {/*    title={'Next'}*/}
-               {/*    color={colors.white}*/}
-               {/*    disabled={currentPage === totalPages - 1 || loadingPlayersGetting}*/}
-               {/*    ml={8}*/}
-               {/*    h={50}*/}
-               {/*    w={100}*/}
-               {/*    onClick={onNextPage}*/}
-               {/*/>*/}
-                <Button
-                    title={'Last'}
-                    color={colors.white}
-                    disabled={currentPage === totalPages - 1 || loadingPlayersGetting}
-                    ml={8}
-                    h={50}
-                    w={100}
+                <Image
+                    w={20}
+                    h={20}
+                    src={'/images/double-arrow-right.png'}
+                    disabled={disableNext}
+                    cursor={'pointer'}
                     onClick={onLastPage}
+                    alt={'double-arrow-right'}
                 />
             </Div>
         </Div>
