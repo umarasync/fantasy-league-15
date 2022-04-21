2b805960fd96409e3d7243dcb62973935ff9bc06
Umar Farooq <umarmughal1194@gmail.com>
2022-04-21 15:02:44 +0500

    save

--- components/playerInfo/BenchBoostModal.js
+++ components/playerInfo/BenchBoostModal.js
@@ -1,44 +1,44 @@
 // Packages
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 // Components
 import PointsBoostModalContent from "components/playerInfo/PointsBoostModalContent";
 
 // Utils
-import {handleBenchBoost} from "utils/chipBoosterHelper";
+import { handleBenchBoost } from "utils/chipBoosterHelper";
 
 // Constants
-import {BOOST_TYPE_BENCH} from "constants/universalConstants";
+import { BOOST_TYPE_BENCH } from "constants/universalConstants";
 
 export default function BenchBoostModal({
-    players,
-    showBenchBoostModal,
-    setShowBenchBoostModal
+  players,
+  showBenchBoostModal,
+  setShowBenchBoostModal,
 }) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  // Global States
+  const user = useSelector(({ auth }) => auth.user);
 
-      // Global States
-      const user = useSelector(({ auth }) => auth.user);
+  const handleBenchBoostConfirmed = async () => {
+    return;
+    await handleBenchBoost({
+      boostType: BOOST_TYPE_BENCH,
+      dispatch,
+      user,
+      setShowModal: setShowBenchBoostModal,
+    });
+  };
 
-    const handleBenchBoostConfirmed = async () => {
-        await handleBenchBoost({
-            boostType: BOOST_TYPE_BENCH,
-            dispatch,
-            user,
-            setShowModal: setShowBenchBoostModal
-        })
-    }
-
-    return (
-        <PointsBoostModalContent
-            title={'Bench boost'}
-            heading={`The points scored by your bench players in the next Gameweek are included in your total.`}
-            subHeading={'(can be used once a season)'}
-            players={players}
-            show={showBenchBoostModal}
-            onCancel={() => setShowBenchBoostModal(false)}
-            onConfirmed={handleBenchBoostConfirmed}
-        />
-    )
+  return (
+    <PointsBoostModalContent
+      title={"Bench boost"}
+      heading={`The points scored by your bench players in the next Gameweek are included in your total.`}
+      subHeading={"(can be used once a season)"}
+      players={players}
+      show={showBenchBoostModal}
+      onCancel={() => setShowBenchBoostModal(false)}
+      onConfirmed={handleBenchBoostConfirmed}
+    />
+  );
 }
--- components/playerInfo/TripleCaptainModal.js
+++ components/playerInfo/TripleCaptainModal.js
@@ -1,44 +1,44 @@
 // Packages
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 // Components
 import PointsBoostModalContent from "components/playerInfo/PointsBoostModalContent";
 
 // Utils
-import {handleBenchBoost} from "utils/chipBoosterHelper";
+import { handleBenchBoost } from "utils/chipBoosterHelper";
 
 // Constants
-import {BOOST_TYPE_TRIPLE_CAPTAIN} from "constants/universalConstants";
+import { BOOST_TYPE_TRIPLE_CAPTAIN } from "constants/universalConstants";
 
 export default function TripleCaptainModal({
-    showTripleCaptainModal,
-    setShowTripleCaptainModal,
-    player,
+  showTripleCaptainModal,
+  setShowTripleCaptainModal,
+  player,
 }) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  // Global States
+  const user = useSelector(({ auth }) => auth.user);
 
-      // Global States
-      const user = useSelector(({ auth }) => auth.user);
+  const handleTripleCaptainConfirmed = async () => {
+    return;
+    await handleBenchBoost({
+      boostType: BOOST_TYPE_TRIPLE_CAPTAIN,
+      dispatch,
+      user,
+      setShowModal: setShowTripleCaptainModal,
+    });
+  };
 
-    const handleTripleCaptainConfirmed = async () => {
-        await handleBenchBoost({
-            boostType: BOOST_TYPE_TRIPLE_CAPTAIN,
-            dispatch,
-            user,
-            setShowModal: setShowTripleCaptainModal
-        })
-    }
-
-    return (
-        <PointsBoostModalContent
-            title={'Triple captain'}
-            heading={`The captain's points will be tripled. You will not be able to change the choice.`}
-            subHeading={`(can be used once a season)`}
-            players={player}
-            show={showTripleCaptainModal}
-            onCancel={() => setShowTripleCaptainModal(false)}
-            onConfirmed={handleTripleCaptainConfirmed}
-        />
-    )
+  return (
+    <PointsBoostModalContent
+      title={"Triple captain"}
+      heading={`The captain's points will be tripled. You will not be able to change the choice.`}
+      subHeading={`(can be used once a season)`}
+      players={player}
+      show={showTripleCaptainModal}
+      onCancel={() => setShowTripleCaptainModal(false)}
+      onConfirmed={handleTripleCaptainConfirmed}
+    />
+  );
 }
