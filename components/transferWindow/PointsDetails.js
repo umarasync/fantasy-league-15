9a6c0dd92d2075499492a49e748d97dd54777ed9
Umar Farooq <umarmughal1194@gmail.com>
2022-04-27 23:49:16 +0500

    save

--- components/transferWindow/PointsDetails.js
+++ components/transferWindow/PointsDetails.js
@@ -6,30 +6,58 @@ import Border from "components/borders/Border";
 // Constants
 import colors from "constants/colors";
 
-export default function PointsDetails({
-    player1,
-    player2
-}) {
-
-    return (
-        <>
-            <Div className={'flex items-center'} mb={26}>
-                <Text text={player1.totalPoints} fs={30} lh={38} fw={600}
-                      className={'text-froly-cerise_red'}
-                      mr={24}
-                />
-                <Border h={70}/>
-                <Text text={player2.totalPoints} fs={30} lh={38} fw={600}
-                      className={'text-froly-cerise_red'}
-                      ml={24}
-                />
-            </Div>
-            <Div className={'flex flex-col items-center'}>
-                <Text text={'Name'} fs={14} lh={18} color={colors.regent_grey} mb={10}/>
-                <Text text={'Next match'} fs={14} lh={18} color={colors.regent_grey} mb={10}/>
-                <Text text={'Price'} fs={14} lh={18} color={colors.regent_grey} mb={10}/>
-                <Text text={'Position'} fs={14} lh={18} color={colors.regent_grey} mb={10}/>
-            </Div>
-        </>
-    )
+export default function PointsDetails({ player1, player2 }) {
+  return (
+    <>
+      <Div className={"flex items-center"} mb={26}>
+        <Text
+          text={player1.totalPoints}
+          fs={30}
+          lh={38}
+          fw={600}
+          className={"text-froly-cerise_red"}
+          mr={24}
+        />
+        <Border h={70} />
+        <Text
+          text={player2.totalPoints}
+          fs={30}
+          lh={38}
+          fw={600}
+          className={"text-froly-cerise_red"}
+          ml={24}
+        />
+      </Div>
+      <Div className={"flex flex-col items-center"}>
+        <Text
+          text={"Name"}
+          fs={14}
+          lh={18}
+          color={colors.regent_grey}
+          mb={10}
+        />
+        <Text
+          text={"Next match"}
+          fs={14}
+          lh={18}
+          color={colors.regent_grey}
+          mb={10}
+        />
+        <Text
+          text={"Price"}
+          fs={14}
+          lh={18}
+          color={colors.regent_grey}
+          mb={10}
+        />
+        <Text
+          text={"Position"}
+          fs={14}
+          lh={18}
+          color={colors.regent_grey}
+          mb={10}
+        />
+      </Div>
+    </>
+  );
 }
